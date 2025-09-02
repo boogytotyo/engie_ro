cd25919f244fb19d115e7c16952b195561d7e567
boogytotyo <33695866+boogytotyo@users.noreply.github.com>
2025-09-03 00:28:28 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,9 @@ from __future__ import annotations
 import pytest
 
 pytest_plugins = "pytest_homeassistant_custom_component"
+
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Auto-enable custom integrations so HA can load our component in tests."""
+    yield
