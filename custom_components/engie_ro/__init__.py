e6ce1248211b17ba0b0f6ecd0342e73b29d6d41b
boogytotyo <33695866+boogytotyo@users.noreply.github.com>
2025-09-02 22:56:04 +0300

    Update __init__.py

--- custom_components/engie_ro/__init__.py
+++ custom_components/engie_ro/__init__.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
-
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-
+from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN
 from .coordinator import EngieDataCoordinator
 
 PLATFORMS: list[str] = ["sensor"]
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coord = EngieDataCoordinator(hass, entry)
@@ -15,7 +12,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await coord.async_config_entry_first_refresh()
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
