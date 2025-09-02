bf1269513d71436b98525b335e83e880a0dbc3de
boogytotyo <33695866+boogytotyo@users.noreply.github.com>
2025-09-03 00:31:32 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,11 @@
 from __future__ import annotations
 
+import pytest  # 👈 asta lipsește
+
 pytest_plugins = "pytest_homeassistant_custom_component"
+
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Activează automat integrațiile custom în teste."""
+    yield
