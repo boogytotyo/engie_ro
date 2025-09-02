c028cf75079cc8048770450c93ad1dcc5df6aaae
boogytotyo <33695866+boogytotyo@users.noreply.github.com>
2025-09-02 16:49:40 +0300

    Update __init__.py

--- custom_components/engie_ro/__init__.py
+++ custom_components/engie_ro/__init__.py
@@ -3,26 +3,22 @@ from __future__ import annotations
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.loader import async_get_integration
 
-from .const import DOMAIN
+from .const import DOMAIN, PLATFORMS
 from .coordinator import EngieDataCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS: list[Platform] = [Platform.SENSOR]
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Setup integrarea dintr-un config entry."""
-    coordinator = EngieDataCoordinator(hass)
+    coordinator = EngieDataCoordinator(hass, entry.data)
     await coordinator.async_config_entry_first_refresh()
 
-    # Citește versiunea din manifest (integration.version)
     integration = await async_get_integration(hass, DOMAIN)
-    version = integration.version  # ex: "1.0.0"
+    version = integration.version  # ex: "2.0.2"
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = {
@@ -35,7 +31,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload integrarea."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data.get(DOMAIN, {}).pop(entry.entry_id, None)
