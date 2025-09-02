af33d6f41a5cd973b466646690b6468eab1364e0
boogytotyo <33695866+boogytotyo@users.noreply.github.com>
2025-09-03 02:18:59 +0300

    Update test_config_flow.py

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from unittest.mock import AsyncMock, patch
 
 import pytest
-from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.engie_ro.const import (
